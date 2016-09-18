81f73495295983633ca91948783c4d61c7e0870d
tagp5 <tagplus5@gmail.com>
2016-09-18 23:15:49 +0300

    update

--- import.sh
+++ import.sh
@@ -5,6 +5,8 @@ FULLPATH=`cd "$BASEDIR"; pwd`
 mkdir -p $FULLPATH/ppa
 cd $FULLPATH/ppa
 
+KEYNAME=350E178B
+
 # code
 wget --content-disposition -N https://go.microsoft.com/fwlink/?LinkID=760868
 
@@ -17,7 +19,8 @@ dpkg-scanpackages . /dev/null |gzip > Packages.gz
 
 apt-ftparchive release . > Release
 gpg --clearsign -o InRelease Release
-gpg -abs -o Release.gpg Release
+#gpg -abs -o Release.gpg Release
+gpg --yes -abs -u $KEYNAME -o Release.gpg Release
 
 #git add *
 #git commit -m 'update'
