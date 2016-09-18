4d4b4c7fa860cc5e474919d6075c2d0dd74a09bd
tagp5 <tagplus5@gmail.com>
2016-09-18 23:49:23 +0300

    update

--- import.sh
+++ import.sh
@@ -13,15 +13,19 @@ wget --content-disposition -N https://go.microsoft.com/fwlink/?LinkID=760868
 # code-insiders 
 wget --content-disposition -N https://go.microsoft.com/fwlink/?LinkID=760865
 
-dpkg-sig --sign builder *.deb
-
 dpkg-scanpackages . /dev/null |gzip > Packages.gz
+
+apt-ftparchive packages . > Packages
+bzip2 -kf Packages
 
 apt-ftparchive release . > Release
 gpg --clearsign -o InRelease Release
 #gpg -abs -o Release.gpg Release
 gpg --yes -abs -u $KEYNAME -o Release.gpg Release
 
-#git add *
-#git commit -m 'update'
-#git push origin master
+#dpkg-sig --sign builder *.deb
+dpkg-sig -k $KEYNAME -s builder *.deb
+
+git add *
+git commit -m 'update'
+git push origin master
