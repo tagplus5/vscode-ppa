2fad4305f8b733419639a231583d910a7bf188fe
tagplus5 <tagplus5@gmail.com>
2016-09-22 22:16:38 +0300

    update

--- update.sh
+++ update.sh
@@ -17,7 +17,7 @@ wget -q --content-disposition -N https://go.microsoft.com/fwlink/?LinkID=760865
 
 COUNT2=`ls -1 | wc -l`
 
-if [ "$COUNT1" != "$COUNT2" ]; then
+#if [ "$COUNT1" != "$COUNT2" ]; then
     
     dpkg-scanpackages -m . /dev/null > Packages
     gzip --keep --force -9 Packages
@@ -26,7 +26,8 @@ if [ "$COUNT1" != "$COUNT2" ]; then
     gpg --yes --clearsign --digest-algo SHA512 -o InRelease Release
     gpg --yes -abs --digest-algo SHA512 -o Release.gpg Release
     
-    git add *
-    git commit -m 'update'
-    git push origin master
-fi;
+    GITBRANCH=`git rev-parse --abbrev-ref HEAD`
+
+    git commit -a -m 'update'
+    git push origin $GITBRANCH
+#fi;
