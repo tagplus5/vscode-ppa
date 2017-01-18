ac7907fdf4e46e184ecede018701473d8bb5a570
tagp5 <andreybbk@gmail.com>
2017-01-18 18:21:34 +0300

    update

--- rm.sh
+++ rm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd 
+cd
 
 git clone --mirror git@github.com:tagplus5/vscode-ppa.git
 
-docker run -it --rm -v `pwd`:/data tagplus5/git-bfg --delete-files code-insiders_1.7* vscode-ppa.git
+docker run -it --rm -v `pwd`:/data tagplus5/git-bfg --delete-files code-insiders_1.8* vscode-ppa.git
 
 cd vscode-ppa.git
 
