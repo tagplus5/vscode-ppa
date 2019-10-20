a6016d0fe7b9817b5861db93897b7f588622a07d
tagplus5 <tagplus5@gmail.com>
2019-10-20 19:23:09 +0000

    update

--- update.sh
+++ update.sh
@@ -17,8 +17,14 @@ wget -q --content-disposition -N https://go.microsoft.com/fwlink/?LinkID=760865
 
 COUNT2=`ls -1 | wc -l`
 
-ls -F code_*.deb | head -n -5 | xargs rm 2> /dev/null
-ls -F code-insiders_*.deb | head -n -5 | xargs rm 2> /dev/null
+for n in 0 1 2 3 4 5 6 7 8 9
+do
+	rename "s/\\.$n\./.0$n./g" *.deb -f
+  rename "s/\\.$n\./.0$n./g" *.deb -f
+done
+
+# ls -F code_*.deb | head -n -5 | xargs rm 2> /dev/null
+# ls -F code-insiders_*.deb | head -n -5 | xargs rm 2> /dev/null
 
 if [ "$COUNT1" != "$COUNT2" ]; then    
   echo 1
