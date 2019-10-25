09bb59934589ea261b745bee2aa5515ec254fe8b
tagplus5 <tagplus5@gmail.com>
2019-10-25 07:02:10 +0000

    update

--- update.sh
+++ update.sh
@@ -15,13 +15,13 @@ wget -q --content-disposition -N https://go.microsoft.com/fwlink/?LinkID=760868
 # code-insiders
 wget -q --content-disposition -N https://go.microsoft.com/fwlink/?LinkID=760865
 
-COUNT2=`ls -1 | wc -l`
-
 for n in 0 1 2 3 4 5 6 7 8 9
 do
 	rename "s/\\.$n\./.0$n./g" *.deb -f
   rename "s/\\.$n\./.0$n./g" *.deb -f
 done
+
+COUNT2=`ls -1 | wc -l`
 
 ls -F code_*.deb | head -n -5 | xargs rm 2> /dev/null
 ls -F code-insiders_*.deb | head -n -5 | xargs rm 2> /dev/null
